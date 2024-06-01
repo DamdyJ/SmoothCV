81a5393ad50291a0fa53d62f08b792aa003ef283
Damdy <damdyjuns@gmail.com>
2024-06-02 00:45:41 +0700

    chore: run npx prisma generate

--- prisma/generated/client/edge.js
+++ prisma/generated/client/edge.js
@@ -189,7 +189,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
--- prisma/generated/client/index.js
+++ prisma/generated/client/index.js
@@ -190,7 +190,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
