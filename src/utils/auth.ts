9a75aa8b0701e2eedd6323200f507375e460d520
Damdy <damdyjuns@gmail.com>
2024-06-02 01:03:57 +0700

    chore: comment out the backend logic

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,15 +1,15 @@
-import { NextAuthOptions } from "next-auth";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { Adapter } from "next-auth/adapters";
-import GoogleProvider from "next-auth/providers/google";
-import prisma from "./prisma";
+// import { NextAuthOptions } from "next-auth";
+// import { PrismaAdapter } from "@auth/prisma-adapter";
+// import { Adapter } from "next-auth/adapters";
+// import GoogleProvider from "next-auth/providers/google";
+// import prisma from "./prisma";
 
-export const authConfig: NextAuthOptions = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-        }),
-    ],
-    adapter: PrismaAdapter(prisma) as Adapter,
-};
+// export const authConfig: NextAuthOptions = {
+//     providers: [
+//         GoogleProvider({
+//             clientId: process.env.GOOGLE_CLIENT_ID as string,
+//             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+//         }),
+//     ],
+//     adapter: PrismaAdapter(prisma) as Adapter,
+// };
