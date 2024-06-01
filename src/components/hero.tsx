bce60f80cb99f4272d8ece893edc35a073878d62
Damdy <damdyjuns@gmail.com>
2024-06-02 00:05:36 +0700

    feat: add reviews component to hero section

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Reviews from "./reviews";
 
 export default function Hero() {
     return (
@@ -7,7 +8,7 @@ export default function Hero() {
                 <div className="mx-auto grid min-h-[calc(100vh-64px)] w-full max-w-7xl grid-rows-2 gap-4 md:grid-cols-12 md:grid-rows-1">
                     <div className="flex h-full w-full items-center justify-center text-left md:col-span-5 lg:col-span-4">
                         <div className="gird w-full text-center md:text-left">
-                            <h1 className="mb-5 text-3xl font-bold lg:text-5xl">
+                            <h1 className="mt-4 mb-5 text-3xl font-bold lg:text-5xl">
                                 The Easy Way to Build Your Perfect Resume
                             </h1>
                             <p className="mb-5">
@@ -17,16 +18,19 @@ export default function Hero() {
                             <button className="btn btn-primary font-semibold">
                                 Join SmoothCV
                             </button>
+                            <div className="mt-12">
+                                <Reviews />
+                            </div>
                         </div>
                     </div>
-                    <div className="grid h-full w-full place-items-center -mt-6 md:mt-0 md:col-span-7 lg:col-span-8">
+                    <div className="grid h-full w-full md:place-items-center md:col-span-7 md:mt-0 md:pt-4 lg:col-span-8">
                         <Image
                             src="/hero.png"
                             alt="hero image"
-                            priority
                             width={1080}
                             height={(928 / 9) * 16}
-                            objectFit="cover"
+                            priority
+                            className="object-cover"
                         />
                     </div>
                 </div>
