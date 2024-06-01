4ad3667db5ed34ed4211456e3f5dcc1e0c80a021
Damdy <damdyjuns@gmail.com>
2024-06-01 17:53:39 +0700

    feat: add hero component into homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import Hero from "@/components/hero";
 import Navbar from "@/components/navbar";
 
 export default function Home() {
     return (
         <>
             <Navbar />
-            
+            <Hero />
         </>
     );
 }
