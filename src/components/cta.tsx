858cc701621d243eb7a54e1b01a60f787c77e42e
Damdy <damdyjuns@gmail.com>
2024-06-02 21:26:31 +0700

    feat: add cta section

---
+++ src/components/cta.tsx
@@ -0,0 +1,22 @@
+export default function CTA() {
+    return (
+        <>
+            <section className="w-full px-4 lg:px-6">
+                <div className="mx-auto w-full max-w-7xl">
+                    <div className="flex flex-col items-center gap-8">
+                        <div className="flex flex-col gap-4 w-full items-center">
+                            <h2 className="w-full md:w-1/2 text-center text-3xl font-extrabold">
+                                "Transform Your Career with SmoothCV: Create
+                                Professional Resumes Effortlessly!"
+                            </h2>
+                            <p>Craft Stunning Resumes in Minutes!</p>
+                        </div>
+                        <button className="btn btn-primary">
+                            Get SmoothCV
+                        </button>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+}
