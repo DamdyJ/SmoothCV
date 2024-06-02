0d9874e41217430335f39def49f30d76db89aeeb
Damdy <damdyjuns@gmail.com>
2024-06-02 20:45:51 +0700

    feat: add all components needed for landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
+import CTA from "@/components/cta";
+import FAQ from "@/components/faq";
+import Features from "@/components/features";
+import Footer from "@/components/footer";
 import Hero from "@/components/hero";
 import Navbar from "@/components/navbar";
+import PainPoint from "@/components/pain-point";
+import Pricing from "@/components/pricing";
+import Testimonial from "@/components/testimonial";
 
 export default function Home() {
     return (
         <>
             <Navbar />
-            <Hero />
+            <div className="flex flex-col gap-8 lg:gap-16">
+                <Hero />
+                <Testimonial index={0}/>
+                <PainPoint />
+                <Testimonial index={1}/>
+                <Features />
+                <Testimonial index={2}/>
+                <Pricing/>
+                <Testimonial index={3}/>
+                <FAQ />
+                <Testimonial index={4}/>
+                <CTA/>
+                <Footer/>
+            </div>
         </>
     );
 }
