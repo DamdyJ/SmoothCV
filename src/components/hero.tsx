b1485b30ee8647d37a0e77a8b525cb6fbe5d3071
Damdy <damdyjuns@gmail.com>
2024-06-02 20:37:06 +0700

    style: add new styling on hero section

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -5,10 +5,10 @@ export default function Hero() {
     return (
         <>
             <div className="w-full px-4 lg:px-6">
-                <div className="mx-auto grid min-h-[calc(100vh-64px)] w-full max-w-7xl grid-rows-2 gap-4 md:grid-cols-12 md:grid-rows-1">
+                <div className="mx-auto flex flex-col min-h-[calc(100vh-64px)] w-full max-w-7xl grid-rows-2 gap-4 md:grid md:grid-cols-12 md:grid-rows-1">
                     <div className="flex h-full w-full items-center justify-center text-left md:col-span-5 lg:col-span-4">
                         <div className="gird w-full text-center md:text-left">
-                            <h1 className="mt-4 mb-5 text-3xl font-bold lg:text-5xl">
+                            <h1 className="mb-5 mt-4 text-3xl font-bold lg:text-5xl">
                                 The Easy Way to Build Your Perfect Resume
                             </h1>
                             <p className="mb-5">
@@ -23,14 +23,14 @@ export default function Hero() {
                             </div>
                         </div>
                     </div>
-                    <div className="grid h-full w-full md:place-items-center md:col-span-7 md:mt-0 md:pt-4 lg:col-span-8">
+                    <div className="grid h-full w-full md:col-span-7 md:mt-0 md:place-items-center md:pt-4 lg:col-span-8">
                         <Image
                             src="/hero.png"
                             alt="hero image"
                             width={1080}
                             height={(928 / 9) * 16}
                             priority
-                            className="object-cover"
+                            className="object-cover transition-transform duration-300 ease-in-out hover:scale-105"
                         />
                     </div>
                 </div>
