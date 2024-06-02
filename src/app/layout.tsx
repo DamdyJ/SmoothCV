78aa307a5b43fa61b3498831f84ef3ea3c095260
Damdy <damdyjuns@gmail.com>
2024-06-02 20:44:15 +0700

    feat: add smooth scrolling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en" data-theme="cupcake">
+        <html lang="en" data-theme="cupcake" className="!scroll-smooth">
             <body className={inter.className}>
                 <main>{children}</main>
             </body>
