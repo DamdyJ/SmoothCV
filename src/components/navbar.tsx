be80de76518e67599710442118bb46680f456b1e
Damdy <damdyjuns@gmail.com>
2024-06-19 18:41:08 +0700

    refactor: reformat navbar component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,105 +1,142 @@
-"use client";
-import { MenuIcon, X } from "lucide-react";
+import { X } from "lucide-react";
 import Link from "next/link";
-import { useState } from "react";
 
 export default function Navbar() {
-    const [open, setOpen] = useState(false);
-
-    const toggleMenu = () => {
-        setOpen(!open);
-    };
-
     return (
         <>
-            <header className="h-16 w-full bg-neutral px-4 text-neutral-content lg:px-6">
-                <nav className="container mx-auto grid h-full max-w-7xl grid-cols-2 items-center justify-items-stretch md:grid-cols-3">
-                    <Link
-                        href="/"
-                        className="btn btn-ghost justify-self-start px-0 text-xl text-primary hover:bg-transparent hover:text-primary"
-                    >
-                        SmoothCV
-                    </Link>
-                    <div className="hidden justify-self-center md:block">
-                        <ul className="flex gap-6">
-                            <li>
-                                <Link
-                                    href="#pricing"
-                                    className="btn btn-ghost hover:bg-transparent hover:font-semibold hover:text-white"
-                                >
-                                    Pricing
-                                </Link>
-                            </li>
-                            <li>
-                                <Link
-                                    href="#faq"
-                                    className="btn btn-ghost hover:bg-transparent hover:font-semibold hover:text-white"
-                                >
-                                    FAQ
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="hidden justify-self-end md:block">
-                        <div className="flex gap-4">
-                            <button className="btn btn-outline btn-primary">
-                                Login
-                            </button>
-                            <button className="btn btn-primary font-semibold">
-                                Join SmoothCV
-                            </button>
-                        </div>
-                    </div>
-                    <div className="-mr-4 justify-self-end md:hidden lg:hidden">
-                        <button
-                            onClick={toggleMenu}
-                            role="button"
-                            className="btn btn-circle btn-ghost hover:text-white"
-                        >
-                            {open ? <X size={24} /> : <MenuIcon size={24} />}
-                        </button>
-                    </div>
-                    {open && (
-                        <div className="absolute right-0 top-0 z-10 min-h-screen w-full bg-white pt-[10px] shadow-lg sm:w-2/3">
-                            <div className="grid">
-                                <button
-                                    onClick={toggleMenu}
-                                    role="button"
-                                    className="btn btn-circle btn-ghost justify-self-end hover:text-base-100"
-                                >
-                                    <X size={24} />
-                                </button>
-                                <ul className="grid gap-4 text-center">
+            <header className="drawer drawer-end z-50">
+                <input
+                    id="my-drawer-3"
+                    type="checkbox"
+                    className="drawer-toggle"
+                />
+                <div className="drawer-content flex flex-col">
+                    {/* Navbar */}
+                    <div className="navbar w-full bg-neutral text-neutral-content">
+                        <div className="mx-auto w-full max-w-7xl">
+                            <div className="mx-2 flex-1 px-2 text-lg font-semibold text-primary md:navbar-start">
+                                <Link href={"/"}>SmoothCV</Link>
+                            </div>
+                            <div className="hidden md:navbar-center md:flex">
+                                <ul className="menu menu-horizontal px-1">
                                     <li>
                                         <Link
-                                            href="/pricing"
-                                            className="btn btn-ghost w-2/3 text-lg hover:text-base-100"
+                                            href={"#pricing"}
+                                            className="btn btn-ghost hover:bg-transparent hover:font-semibold hover:text-white"
                                         >
                                             Pricing
                                         </Link>
                                     </li>
                                     <li>
                                         <Link
-                                            href="/faq"
-                                            className="btn btn-ghost w-2/3 text-lg hover:text-base-100"
+                                            href={"#faq"}
+                                            className="btn btn-ghost hover:bg-transparent hover:font-semibold hover:text-white"
                                         >
                                             FAQ
                                         </Link>
                                     </li>
                                 </ul>
-                                <div className="mx-auto my-4 w-2/3 border-b border-primary"></div>
-                                <div className="mx-auto grid w-2/3 gap-4">
-                                    <button className="btn btn-outline btn-primary">
-                                        Login
-                                    </button>
-                                    <button className="btn btn-primary font-semibold">
-                                        Get SmoothCV
-                                    </button>
-                                </div>
+                            </div>
+                            <div className="flex-none md:hidden">
+                                <label
+                                    htmlFor="my-drawer-3"
+                                    aria-label="open sidebar"
+                                    className="btn btn-square btn-ghost"
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        className="inline-block h-6 w-6 stroke-current"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            d="M4 6h16M4 12h16M4 18h16"
+                                        ></path>
+                                    </svg>
+                                </label>
+                            </div>
+
+                            <div className="hidden flex-none md:navbar-end sm:items-end md:flex md:flex-1">
+                                <ul className="menu menu-horizontal space-x-4">
+                                    {/* Navbar menu content here */}
+                                    <li>
+                                        <Link
+                                            href={"/"}
+                                            className="btn btn-outline btn-primary"
+                                        >
+                                            Login
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link
+                                            href={"/"}
+                                            className="btn btn-primary"
+                                        >
+                                            Get SmoothCV
+                                        </Link>
+                                    </li>
+                                </ul>
                             </div>
                         </div>
-                    )}
-                </nav>
+                    </div>
+                </div>
+                <div className="drawer-side">
+                    <label
+                        htmlFor="my-drawer-3"
+                        aria-label="close sidebar"
+                        className="drawer-overlay"
+                    ></label>
+                    <ul className="menu min-h-full w-80 space-y-4 bg-white p-4">
+                        {/* Sidebar content here */}
+                        <div className="flex items-center justify-between">
+                            <span className="w-fit text-2xl font-semibold">
+                                Menu
+                            </span>
+                            <label
+                                htmlFor="my-drawer-3"
+                                aria-label="close sidebar"
+                                className="btn btn-ghost rounded-lg"
+                            >
+                                <X size={24} />
+                            </label>
+                        </div>
+                        <li>
+                            <Link
+                                href={"#pricing"}
+                                className="btn btn-ghost text-lg hover:text-black"
+                            >
+                                Pricing
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href={"#faq"}
+                                className="btn btn-ghost text-lg hover:text-black"
+                            >
+                                FAQ
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href={"/"}
+                                className="btn btn-outline btn-primary text-lg"
+                            >
+                                Login
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href={"/"}
+                                className="btn btn-primary text-lg"
+                            >
+                                Get SmoothCV
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
             </header>
         </>
     );
